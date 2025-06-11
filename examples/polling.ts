0bc48961045dd42c37b3e9649d5b3ba86fc3b187
koo0ki <kakoytoaleksandr@gmail.com>
2025-06-11 18:38:40 +0700

    fix polling && add examples

---
+++ examples/polling.ts
@@ -0,0 +1,42 @@
+import { Bot, InlineKeyboard, Keyboard } from 'grammy';
+import { CryptoPayClient } from '../src';
+
+const bot = new Bot(Bun.env.BOT_TOKEN!);
+const cryptoPay = new CryptoPayClient({
+    token: Bun.env.CRYPTO_PAY_TOKEN!,
+    net: 'testnet',
+    pollingEnabled: true,
+    pollingInterval: 5000
+});
+
+bot.command('start', ctx => {
+    ctx.reply('Hello from donate bot', {
+        reply_markup: new Keyboard().text('Donate').resized()
+    });
+});
+
+bot.hears('Donate', async ctx => {
+    const invoice = await cryptoPay.createInvoice({
+        amount: '10',
+        currency_type: 'crypto',
+        asset: 'USDT'
+    });
+
+    cryptoPay.polling.add({
+        result: invoice.result!,
+        userId: ctx.from!.id.toString(),
+        endTimestamp: Date.now() + 60000 * 10
+    });
+
+    ctx.reply('Pay 10$', {
+        reply_markup: new InlineKeyboard().url('Pay', invoice.result?.bot_invoice_url!)
+    });
+});
+
+cryptoPay.polling.on('invoicePaid', async invoice => {
+    bot.api.sendMessage(invoice.userId, `your donate ${invoice.result.amount} has been received`).catch(() => {});
+});
+
+bot.start();
+await bot.init();
+console.log(`${new Date().toISOString()} - ${bot.botInfo.username} started`);
---
+++ examples/webhook.ts
@@ -0,0 +1,40 @@
+import { Bot, InlineKeyboard, Keyboard } from 'grammy';
+import { CryptoPayClient, Webhook } from '../src';
+
+const bot = new Bot(Bun.env.BOT_TOKEN!);
+const cryptoPay = new CryptoPayClient({
+    token: Bun.env.CRYPTO_PAY_TOKEN!,
+    net: 'testnet'
+});
+
+const webhook = new Webhook(Bun.env.CRYPTO_PAY_TOKEN!);
+
+bot.command('start', ctx => {
+    ctx.reply('Hello from donate bot', {
+        reply_markup: new Keyboard().text('Donate').resized()
+    });
+});
+
+bot.hears('Donate', async ctx => {
+    const invoice = await cryptoPay.createInvoice({
+        amount: '10',
+        currency_type: 'crypto',
+        asset: 'USDT',
+        payload: ctx.from!.id.toString()
+    });
+
+    ctx.reply('Pay 10$', {
+        reply_markup: new InlineKeyboard().url('Pay', invoice.result?.bot_invoice_url!)
+    });
+});
+
+webhook.on('update', async update => {
+    bot.api
+        .sendMessage(String(update.payload?.payload), `your donate ${update.payload.amount} has been received`)
+        .catch(() => {});
+});
+
+webhook.start();
+bot.start();
+await bot.init();
+console.log(`${new Date().toISOString()} - ${bot.botInfo.username} started`);
